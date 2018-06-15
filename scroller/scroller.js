d4759351fa93eed5cb1856b9d2ea144e1d3d9801
Jérôme <jerome@fourmond.fr>
2018-06-15 16:15:16 +0200

    Update scroller.js

--- scroller/scroller.js
+++ scroller/scroller.js
@@ -1,21 +1,28 @@
 const windowHeight = window.innerHeight;
 const scrollHeight = document.body.scrollHeight - windowHeight;
 
-function scrollTransition(ease) {
+function scrollTransition(ease, direction='bottom') {
+    var Y = window.pageYOffset || document.documentElement.scrollTop;
+
     d3.transition().duration(5000).ease(ease)
         .tween('scroll', () => {
-            const i = d3.interpolate(0, scrollHeight);
+            let i = null;
+            if(direction === 'top')
+                i = d3.interpolate(scrollHeight, 0);
+            else if(direction === 'bottom')
+                i = d3.interpolate(0, scrollHeight);
             return function (t) {
                 window.scroll(0, i(t));
             }
         })
         .on('start', () => {
-            console.log('TRANSITION STARTED');
+            d3.selectAll('button').attr('disabled', true);
         })
         .on('interrupt', () => {
             console.log('TRANSITION INTERRUPTED');
+            d3.selectAll('button').attr('disabled', null);
         })
         .on('end', () => {
-            console.log('TRANSITION ENDED');
+            d3.selectAll('button').attr('disabled', null);
         });
 }

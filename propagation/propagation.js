e448df33ec31c0e2120beb4cc5f35d66e62b863d
jfourmond <jerome@fourmond.fr>
2019-04-23 17:10:04 +0200

    Updated propagation viz

--- propagation/propagation.js
+++ propagation/propagation.js
@@ -1,103 +1,176 @@
-const width = window.innerWidth;
-const height = window.innerHeight;
+const butProp = document.getElementById("butProp");
+
+const height = 500;
+const width = 960;
 
 const svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-const line = d3.line()
-    .x((d) => d[0])
-    .y((d) => d[1]);
+    .attr("width", 960)
+    .attr("height", 500);
 
 const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-const data = [
-    [[200, 200], [200, 300], [200, 400], [200, 500]],  // Première couche
-    [[550, 250], [550, 350], [550, 450]],  // Seconde couche
-    [[900, 200], [900, 300], [900, 400], [900, 500]]  // Troisième couche
-];
+let simulation = null;
 
-const layers_circles = [];
-const layers_connexions_static = [];
-const layers_connexions_animated = [];
+let node = null;
+let link = null;
 
-for (let layers_idx = 0; layers_idx < (data.length - 1); layers_idx++) {
-    const connexions_static = [ ];
-    const connexions_animated = [ ];
-    for (let units1_idx = 0; units1_idx < (data[layers_idx].length); units1_idx++) {
-        const units_1 = data[layers_idx][units1_idx];
+let nLayer = 0;
 
-        for (let units2_idx = 0; units2_idx < (data[layers_idx + 1].length); units2_idx++) {
-            const units_2 = data[layers_idx + 1][units2_idx];
+let transitions = 0;
 
-            let link = [units_1, units_2];
+Promise.all([
+    d3.json("../data/propagation/nodes.json"),
+    d3.json("../data/propagation/links.json")
+]).then((values) => {
+    for (let i = 0; i < values[1].length; i++)
+        values[1][i].weight = Math.random();
 
-            let path = svg.append("path")
-                .attr("d", line(link))
-                .attr("fill", "none")
-                .attr("stroke", "#cccccc")
-                .attr("stroke-linejoin", "round")
-                .attr("stroke-linecap", "round")
-                .attr("stroke-width", 1);
-            connexions_static.push({source: units_1, destination: units_2, link: path});
+    nLayer = d3.max(values[0], function (x) { return x.layer })
 
-            let totalLength = path.node().getTotalLength();
-            let dashLength = totalLength / 16;
-
-            let animated_path = svg.append("path")
-                .attr("d", line(link))
-                .attr("fill", "none")
-                .attr("stroke", "orange")
-                .attr("stroke-linejoin", "round")
-                .attr("stroke-linecap", "round")
-                .attr("stroke-width", 1.5)
-                .attr("stroke-dasharray", dashLength + " " + (totalLength + dashLength))
-                .attr("stroke-dashoffset", dashLength)
-                .attr("stroke-linecap", "round");
-
-            animated_path.animate = () => {
-                return animated_path.transition()
-                    .duration(1000).ease(d3.easeLinear)
-                    .attr("stroke-dashoffset", -(totalLength))
-            };
-
-            connexions_animated.push({
-                source:units1_idx,
-                destination: units2_idx,
-                link: animated_path
-            });
-        }
-    }
-    layers_connexions_static.push(connexions_static);
-    layers_connexions_animated.push(connexions_animated);
-}
-
-for (let idx = 0; idx < data.length; idx++) {
-    const d = data[idx];
-
-    layers_circles.push(
-        svg.selectAll(".layer_" + idx)
-            .data(d)
-            .enter()
-            .append("circle")
-            .attr("class", "layer_" + idx)
-            .attr("id", (d, i) => "unit_" + i)
-            .attr("r", 20)
-            .attr("fill", color(idx))
-            .attr("cx", (d) => d[0])
-            .attr("cy", (d) => d[1])
-    );
-}
-
-// Animation de la première couche
-// layers_connexions_animated[0].forEach(c => c.link.animate());
-
-// Animation des liens
-d3.selectAll(".layer_0").filter("#unit_1").each(x => {
-    layers_connexions_animated[0].filter(c => c.source == 1)
-        .forEach(c => c.link.animate().on('end', () => {
-            layers_connexions_animated[1].filter(c1 => c1.destination == c.destination)
-                .forEach(c1 => c1.link.animate());
-        }));
+    build(values[0], values[1]);
+}).catch((e) => {
+    console.error(e);
 });
 
+function activationFunction(value) {
+    // RELU
+    return Math.max(0, value);
+}
+
+function build(nodes, links) {
+    simulation = d3.forceSimulation(nodes)
+        .force("link", d3.forceLink(links).id(d => d.id).strength(0));
+
+    link = svg.append("g")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
+        .selectAll("line")
+        .data(links)
+        .join("line")
+        .attr("stroke-width", d => 0.5).attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
+
+    node = svg.append("g")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
+        .selectAll("circle")
+        .data(nodes)
+        .join("circle")
+        .attr("r", 15)
+        .attr("fill", (d, i) => color(d.layer))
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+        .attr("value", function (d) {
+            if (d.layer == 1) return 1;
+            return 0;
+        });
+}
+
+d3.selectAll('input').on('input', function (d, i) {
+    let value = parseFloat(d3.select(this).property("value"));
+    if (isNaN(value))
+        value = 0;
+
+    node.filter((n) => n.layer == 1 && n.unit == (i + 1))
+        .attr("value", value)
+        .transition().duration(500).attr("fill", function (d) {
+            let ncolor = d3.color(color(d.layer));
+            ncolor.opacity = value;
+            return ncolor + ""
+        }).attr("stroke", function () {
+            if (value < 0.5) return '#000';
+            else return '#fff';
+        });
+});
+
+function animate(selection) {
+    selection.each(function (d, i) {
+        let value = parseFloat(d3.select(this).attr("value"));
+
+        link.filter((li) => li.source == d).each((d1, i) => {
+            const src = d;
+            const weight = d1.weight;
+            const dst = d1.target;
+
+            svg.append("line")
+                .attr("x1", src.x)
+                .attr("y1", src.y)
+                .attr("x2", src.x)
+                .attr("y2", src.y)
+                .attr("stroke", "#fe2d2d")
+                .attr("stroke-width", 1.75)
+                .style("opacity", weight * value)
+                .lower()	// Placement en arrière plan
+                .transition().duration(1000).ease(d3.easeLinear)
+                .attrTween("x1", function () {
+                    const x = d3.interpolateNumber(src.x, dst.x);
+                    return function (t) {
+                        let nxt = x(t - 0.05);
+                        if ((t - 0.05) < 0) nxt = src.x;
+                        return nxt;
+                    };
+                })
+                .attrTween("y1", function () {
+                    const y = d3.interpolateNumber(src.y, dst.y);
+                    return function (t) {
+                        let nxt = y(t - 0.05);
+                        if ((t - 0.05) < 0) nxt = src.y;
+                        return nxt;
+                    };
+                })
+                .attr("x2", dst.x)
+                .attr("y2", dst.y)
+                .on('start', function () {
+                    transitions++;
+                })
+                .on('end', function () {
+                    transitions--;
+
+                    d3.select(this).remove();
+
+                    node.filter((n) => n == d1.target).attr('value', function () {
+                        return parseFloat(d3.select(this).attr('value')) + d1.weight * value;
+                    })
+
+                    node.filter((n) => n == d1.target)
+                        .transition().duration(100).attr("r", 17)
+                        .transition().duration(100).attr("r", 15);
+
+                    if (transitions == 0) {
+                        node.filter((n) => n.layer == (d.layer + 1)).attr('value', function () {
+                            return activationFunction(d3.select(this).attr('value'));
+                        }).call(animate);
+
+                        if (d1.target.layer == nLayer) {
+                            // TODO animation de la prédiction
+                            let xSelect = node.filter((n) => n.layer == (d.layer + 1));
+                            let xSelectNodes = xSelect.nodes();
+
+                            let xS = d3.scan(xSelect.nodes(), (a, b) => b.attributes.value.value - a.attributes.value.value);
+
+                            d3.select(xSelectNodes[xS])
+                                .transition().duration(150)
+                                .attr('fill', 'yellow')
+                                .attr('stroke', 'red');
+
+                            butProp.disabled = false;
+                        }
+                    }
+                });
+        });
+    })
+}
+
+function propagate() {
+    butProp.disabled = true
+    // Initialisation
+    node.filter((n) => n.layer != 1)
+        .attr('value', 0)
+        .transition().duration(100)
+        .attr("fill", (d, i) => color(d.layer))
+        .attr('stroke', '#fff');
+    // Récupération des noeuds de la première couche et activation de la propagation
+    node.filter((n) => n.layer == 1).call(animate);
+}
